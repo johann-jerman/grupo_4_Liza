c86fd5492df52664d1e9997be2ffcf45d19a86b9
unknown <johannjerman014@gmail.com>
2023-01-11 23:09:41 -0300

    header y footer prototipo

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ app.use(express.static('public'));
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, './views/index.html'))
 });
+app.get('/men', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/men.html'))
+});
+app.get('/women', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/women.html'))
+});
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+app.get('/register', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+app.get('/product', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/product.html'))
+});
+app.get('/shopping-cart', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/shopping-cart.html'))
+});
 
 app.listen(PORT, ()=>{
     console.log(`server corrinedo en ${PORT}`);
