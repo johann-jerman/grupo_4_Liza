59ced7bdd36d46f9a7ebd9d41e13393f5311c705
unknown <johannjerman014@gmail.com>
2023-01-18 16:24:38 -0300

    mejorando hombres y mujeres

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3004
+const PORT = process.env.PORT || 3004
 
 app.use(express.static('public'));
 
