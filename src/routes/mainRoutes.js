01c6003cbf165dec9441ab00b3c4b405878f1677
unknown <johannjerman014@gmail.com>
2023-01-28 19:20:36 -0300

    agregado de ejs

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const mainController = require('../controller/mainController');
+
+router.get('/',)
 
 module.exports = router
