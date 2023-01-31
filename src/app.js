a0a4f6300001dbf2b983d50d52a23e83acc92881
unknown <johannjerman014@gmail.com>
2023-01-31 16:58:37 -0300

    agregando carpeta user/product

--- src/app.js
+++ src/app.js
@@ -11,28 +11,7 @@ app.set('views', path.resolve(__dirname, 'views'));
 
 app.use(mainRoutes)
 
-// definiendo rutas
-// app.get('/', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, './views/index.html'))
-// });
-// app.get('/men', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, './views/men.html'))
-// });
-// app.get('/women', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, './views/women.html'))
-// });
-// app.get('/login', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, './views/login.html'))
-// });
-// app.get('/register', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, './views/register.html'))
-// });
-// app.get('/product', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, './views/product.html'))
-// });
-// app.get('/shopping-cart', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, './views/shopping-cart.html'))
-// });
+
 
 app.listen(PORT, ()=>{
     console.log(`server corriendo en http://localhost:${PORT}`);
--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -1,36 +1,36 @@
 const mainController = {
     home : (req, res) => {
         res.render('index',{
-            css: './css/styles.css'
+            css: './css/styles.css',
         })
     },
     login : (req, res) => {
-        res.render('login',{
+        res.render('./users/login',{
             css: './css/login.css'
         })
     },
     register : (req, res) => {
-        res.render('register',{
+        res.render('./users/register',{
             css: './css/register.css'
         })
     },
     man : (req, res) => {
-        res.render('man',{
+        res.render('./products/man',{
             css: './css/genre.css'
         })
     },
     women : (req, res) => {
-        res.render('women',{
+        res.render('./products/women',{
             css: './css/genre.css'
         })
     },
     product : (req, res) => {
-        res.render('product',{
+        res.render('./products/product',{
             css: './css/product.css'
         })
     },
     shoppingCart : (req, res) => {
-        res.render('shopping-cart',{
+        res.render('./products/shopping-cart',{
             css: './css/shopping-cart.css'
         })
     },
