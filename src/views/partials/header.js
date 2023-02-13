6834dcab619757d804bf6ab4e16db6f0ba279d8a
Stipideses <stip.stip@yahoo.es>
2023-02-13 13:05:17 -0500

    Agregando  estilos header

---
+++ src/views/partials/header.js
@@ -0,0 +1,17 @@
+// const promotion-bar = header.querySelector("promotion-bar");
+// const promotion1 = header.querySelector("promotion1");
+// const promotion2 = header.querySelector("promotion2");
+
+// function showpromotion1() {
+//     promotion1.style.display = "block";
+//     promotion2.style.display = "none";
+//   setTimeout(showpromotion2, 10000);
+// }
+
+// function showpromotion2() {
+//     promotion1.style.display = "none";
+//     promotion2.style.display = "block";
+//   setTimeout(showpromotion1, 10000);
+// }
+
+// showpromotion1();
