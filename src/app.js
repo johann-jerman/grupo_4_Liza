87ea3bd9afbd4546b9fec53a63fd61ac70e71e5d
unknown <johannjerman014@gmail.com>
2023-02-27 10:45:34 -0300

    agregando bcrypt

--- src/app.js
+++ src/app.js
@@ -26,8 +26,8 @@ app.use('/product', productRoutes);
 app.use('/user', userRoutes);
 
 app.use((req, res, next)=>{
-res.status(404).render('error' , {css:null});
-next();
+    res.status(404).render('error' , {css:null});
+    next();
 })
 
 
