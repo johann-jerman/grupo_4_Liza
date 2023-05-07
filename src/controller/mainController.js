60bd3fc9b3ab6173b6602bed67dec893de28828e
Stipideses <stip.stip@yahoo.es>
2023-05-07 17:20:56 -0500

    detalles header y footer

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -31,16 +31,16 @@ const mainController = {
             let SizeProduct = await db.SizeProduct.findAll()
     
             res.json( {
-                // colorProduct,
-                // imageProduct,
-                // SizeProduct,
-                // producto,
+                colorProduct,
+                imageProduct,
+                SizeProduct,
+                producto,
                 user,
                 userCategory,
                 category,
                 color,
                 size,
-                // image
+                image
             })
             
         } catch (error) {
